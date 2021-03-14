f98c22b74d747242174ca98ca10256d4d097fc57
nadiruzzamanmuhammad <nadiruzzamanqa@gamil.com>
2021-03-14 10:37:37 -0400

    Find Webelements  by xpath and Id

--- src/test/java/loginTest/ValidLoginTest.java
+++ src/test/java/loginTest/ValidLoginTest.java
@@ -17,11 +17,11 @@ public class ValidLoginTest extends ScriptBase {
     @Test
     public void verifySignInButtonDisplayed() throws InterruptedException {
 
-    //driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a")).click();
-        //driver.findElement(By.id("search_query_top")).sendKeys("Badol Vai");
-        //driver.findElement(By.xpath("//*[@id='search_query_top']")).sendKeys("Ripon vai");
-        //Thread.sleep(5000);
-        //driver.findElement(By.xpath("//*[@id='search_query_top']")).clear();
+    driver.findElement(By.xpath("//*[@id='header']/div[2]/div/div/nav/div[1]/a")).click();
+        driver.findElement(By.id("search_query_top")).sendKeys("Badol Vai");
+        driver.findElement(By.xpath("//*[@id='search_query_top']")).sendKeys("Ripon vai");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id='search_query_top']")).clear();
 
         WebElement element=driver.findElement(By.xpath("//*[@id='contact-link']/a"));
         element.isDisplayed();
